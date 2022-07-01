670470c303dec2502b2639b996d0c5eb28441d8e
oguze <oguze@devbox>
2022-07-01 18:55:12 +0300

    Add project files.

---
+++ Faktöriyel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+// namespace Faktöriyel
+//{
+
+// public class Program
+//{
+
+//  static int faktoriyel (int n)
+//{
+//  if (sayi <= 1)
+// {
+//  return 1;
+// }
+// else
+// {
+// return sayi * faktoriyel(sayi - 1);
+// }
+//}
+//}
+//}
+
+
+namespace forFaktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel *= i;
+            }
+            Console.WriteLine(sayi + "! = " + faktoriyel);
+
+        }
+    }
+}
+namespace whileFaktoriyel
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            while (sayi > 0)
+            {
+                factorial *= sayi;
+                sayi--;
+            }
+            Console.WriteLine(sayi + "!=" + factorial);
+        }
+
+    }
+
+}
+    
